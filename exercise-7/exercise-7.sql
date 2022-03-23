2cb857d75beec2ac208917d483a49d54cf636c48
joshua360x <jkw5499@psu.edu>
2022-03-23 17:35:16 -0400

    added test 7 successfully

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,14 +1,14 @@
--- -- customer ranking by total film time watched
--- -- similar to exercise 6
--- SELECT
--- customer.email,
--- RANK() OVER (ORDER BY SUM(film.length) DESC)
--- FROM
--- customer
--- LEFT JOIN rental
--- ON customer.customer_id = rental.customer_id
--- LEFT JOIN inventory
--- ON rental.inventory_id = inventory.inventory_id
--- LEFT JOIN film
--- ON inventory.film_id = film.film_id
--- GROUP BY customer.email;
+-- customer ranking by total film time watched
+-- similar to exercise 6
+SELECT
+customer.email,
+RANK() OVER (ORDER BY SUM(film.length) DESC)
+FROM
+customer
+LEFT JOIN rental
+ON customer.customer_id = rental.customer_id
+LEFT JOIN inventory
+ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN film
+ON inventory.film_id = film.film_id
+GROUP BY customer.email;
